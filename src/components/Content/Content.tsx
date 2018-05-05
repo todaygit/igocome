da7f17a435504d9b1d7bea13260a5e307d45d92e
my-dev-world <vojmil.radic@gmail.com>
2018-05-05 12:31:30 -0500

    first page

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import "./Content.scss";
-
+const ruby = require("../../assets/img/me/ruby.png");
+const phone = require("../../assets/img/me/phone.png");
+const routes = require("../../assets/img/me/routes.png");
 export default class Content extends React.Component<{}, undefined> {
   render() {
     return (
@@ -11,18 +13,18 @@ export default class Content extends React.Component<{}, undefined> {
         <p className="exp">For the best experience, please use your mobile phone when searching the best deals.</p>
         <div className="show">
           <div className="cont no1">
-            <img src="https://dummyimage.com/45" alt="ruby" className="mark"/>
+            <img src={ruby} alt="ruby" className="mark"/>
             <h3 className="sub">All Providers on one site</h3>
             <p className="content">All major Transport providers are listed
 on iGoCome, which means no more searching across multiple sites to find your trip.</p>
           </div>
           <div className="cont no2">
-            <img src="https://dummyimage.com/45" alt="phone" className="mark"/>
+            <img src={phone} alt="phone" className="mark"/>
             <h3 className="sub">Mobile Friendly</h3>
             <p className="content">iGoCome has been specifically designed for mobile, allowing you to find the best deals, wherever you are!</p>
           </div>
           <div className="cont no3">
-            <img src="https://dummyimage.com/45" alt="routes" className="mark"/>
+            <img src={routes} alt="routes" className="mark"/>
             <h3 className="sub">All routes covered</h3>
             <p className="content">Every route has been captured by iGoCome, allowing you to travel further than ever at the best possible price.</p>
           </div>
