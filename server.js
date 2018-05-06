70e262e982a5ca038b0dc3762d4672910ca8da04
my-dev-world <vojmil.radic@gmail.com>
2018-05-05 21:49:05 -0500

    responsive updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express();
 app.use(express.static('dist'));
+app.use(express.static('./src/assets'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Servier is listening on ' + port));
