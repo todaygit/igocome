9e53fcdbdc014e1e757d39fa160821477e1bd39c
my-dev-world <vojmil.radic@gmail.com>
2018-05-07 02:48:59 -0500

    material UI ready to use

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -11,24 +11,24 @@ export default class Content extends React.Component<{}, undefined> {
       <div className="content">
         <h1 className="title">SEARCH ALL ROUTES AT ONCE</h1>
         <p className="acc">Find the best deals across all Providers in one location.</p>
-        <button className="started hvr-rectangle-out">Get Started</button>
+        <button className="started hvr-float-shadow">Get Started</button>
         <p className="exp">For the best experience, please use your mobile phone when searching the best deals.</p>
         <div className="al-show">
           <div className="cont no1">
-            <img src={ruby} alt="ruby"/>
+            <img src={ruby} alt="ruby" />
             <h3 className="sub">All Providers on one site</h3>
             <p className="content">All major Transport providers are listed
 on iGoCome, which means no more searching across multiple sites to find your trip.</p>
           </div>
           <div className="cont no2">
-            <img src={phone} alt="phone"/>
+            <img src={phone} alt="phone" />
             <h3 className="sub">Mobile Friendly</h3>
             <p className="content">iGoCome has been specifically designed for mobile, allowing you to find the best deals, wherever you are!</p>
           </div>
           <div className="cont no3">
             <img src={routes} alt="routes"/>
             <h3 className="sub">All routes covered</h3>
-            <p className="content">Every route has been captured by iGoCome, allowing you to travel further than ever at the best possible price.</p>
+            <p className="content">Every r oute has been captured by iGoCome, allowing you to travel further than ever at the best possible price.</p>
           </div>
         </div>
       </div>
