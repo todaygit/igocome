ab1674968545d841cf1846d0436ba9dd7de6ef3e
todaycode <onetoday1030@gmail.com>
2018-05-17 12:14:35 -0500

    fixed for Homepage

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -11,7 +11,7 @@ export default class Content extends React.Component<{}, undefined> {
       <div className="content">
         <h1 className="title">SEARCH ALL ROUTES AT ONCE</h1>
         <p className="acc">Find the best deals across all Providers in one location.</p>
-        <button className="started hvr-float-shadow">Get Started</button>
+        <button className="started">Get Started</button>
         <p className="exp">For the best experience, please use your mobile phone when searching the best deals.</p>
         <div className="al-show">
           <div className="cont no1">
@@ -23,12 +23,12 @@ on iGoCome, which means no more searching across multiple sites to find your tri
           <div className="cont no2">
             <img src={phone} alt="phone" />
             <h3 className="sub">Mobile Friendly</h3>
-            <p className="content">iGoCome has been specifically designed for mobile, allowing you to find the best deals, wherever you are!</p>
+            <p className="content">iGoCome has been specifically designed for mobile, allowing you to find the best deals, wherever you are!</p>
           </div>
           <div className="cont no3">
             <img src={routes} alt="routes"/>
             <h3 className="sub">All routes covered</h3>
-            <p className="content">Every r oute has been captured by iGoCome, allowing you to travel further than ever at the best possible price.</p>
+            <p className="content">Every route has been captured by iGoCome, allowing you to travel further than ever at the best possible price.</p>
           </div>
         </div>
       </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Button from 'material-ui/Button';
 import Menu, { MenuItem } from 'material-ui/Menu';
 import "./Header.scss";
 import "./responsive.scss";
 
 const logo = require("./assets/img/page-1.png");
+const style = {
+  transform: 'translateY(50px)'
+};
 
 export default class Header extends React.Component<{},any> {
   constructor(props) {
@@ -16,7 +21,11 @@ export default class Header extends React.Component<{},any> {
     this.handleClose = this.handleClose.bind(this);
   }
   handleClick(event) {
-    this.setState({ anchorEl: event.currentTarget });
+    if (this.state.anchorEl) {
+      this.setState({ anchorEl: null });
+    } else {
+      this.setState({ anchorEl: event.currentTarget });
+    }
   }
 
   handleClose() {
@@ -25,27 +34,28 @@ export default class Header extends React.Component<{},any> {
   render() {
     const { anchorEl } = this.state;
     return (
-      <div className="header">
+      <div className="header" id="header">
         
         <img src={logo} width="108" height="54" />
         <ul className="menu">
           <li>Contact</li>
           <li>About</li>
         </ul>
-        <button className="igo hvr-forward">iGoCome for Business</button>
+        <button className="igo">iGoCome for Business</button>
         <Button
           aria-owns={anchorEl ? 'simple-menu' : null}
           aria-haspopup="true"
           onClick={this.handleClick}
           className="bars"
         >
-          <i className="fa fa-bars" aria-hidden="true"></i>
+          <i className={"fa "+(this.state.anchorEl ? 'fa-close' : 'fa-bars')} aria-hidden="true"></i>
         </Button>
         <Menu
           id="simple-menu"
           anchorEl={anchorEl}
           open={Boolean(anchorEl)}
           onClose={this.handleClose}
+          style={style}
         >
           <MenuItem onClick={this.handleClose}>Profile</MenuItem>
           <MenuItem onClick={this.handleClose}>My account</MenuItem>
